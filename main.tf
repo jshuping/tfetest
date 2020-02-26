e3309bbc9aee6170236e5a7f9eda5ede845d8337
John Shuping <jshuping@acm.org>
2020-02-26 13:28:33 -0600

    test3

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
 variable "foo" {
 }
 
+module "test" {
+  source  = "app.terraform.io/ebtest/test/null"
+  version = "1.0.0"
+  test_string = "yeehaw"
+}
+
+output "modtest" {
+    value = "${module.test.test_output}"
+}
+
 resource "null_resource" "slack_channels" {
   provisioner "local-exec" {
     command = "echo hello world ${var.foo}"
